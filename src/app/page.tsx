861f47e4a03ff5c58599897f5b158cb3eab02c25
HaRish46 <43755251+HaRish46@users.noreply.github.com>
2024-11-03 14:59:20 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,24 @@ import DataCard from "@/components/DataCard";
 import mainIcon from "../public/main-logo-1.png";
 import { getCareers } from "../actions/careerOverviewAction";
 
-const Home = async () => {
-  const { data, errMsg } = await getCareers();
+// Define the type for the career option object
+interface CareerOption {
+  id: string,
+  name: string;
+  image: string;
+  description: string;
+  redirectPageName?: string;
+}
+
+interface CareersResponse {
+  data?: CareerOption[] | null;
+  errMsg?: string;
+}
+
+// The Home component is an async function returning JSX
+const Home: React.FC = async () => {
+  const { data }: CareersResponse = await getCareers();
+
   return (
     <div className="p-3 w-full" style={{ marginTop: "5%" }}>
       <div className="flex flex-col">
@@ -24,8 +40,8 @@ const Home = async () => {
             />
           </div>
         </div>
-        <div className="bg-zinc-5 rounded border-2 border-gray-500  p-4 flex flex-wrap gap-x-7 gap-y-4 mb-3 justify-items-start">
-          {data?.map((careerOption) => {
+        <div className="bg-zinc-5 rounded border-2 border-gray-500 p-4 flex flex-wrap gap-x-7 gap-y-4 mb-3 justify-items-start">
+          {data?.map((careerOption: CareerOption) => {
             return (
               <DataCard careerOption={careerOption} key={careerOption.id} />
             );
