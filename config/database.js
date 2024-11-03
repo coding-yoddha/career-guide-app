cd74460b603e8889290dad96db4c91489900d503
Krishnama Naidu <krishnamkrish67@gmail.com>
2024-11-03 17:19:58 +0530

    test

--- config/database.js
+++ config/database.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  if(mongoose.connections[0].readyState){
+  if (mongoose.connections[0].readyState) {
     return true;
   }
 
   try {
+    console;
+    console.log("Mongodb connection test ", process.env.MONGODB_URL);
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log('Mongodb connected')
+    console.log("Mongodb connected");
     return true;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 export default connectDB;
