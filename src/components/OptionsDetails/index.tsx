3c29fd09195c8511afea476c82df892d3e4bba54
Krishnama Naidu <krishnamkrish67@gmail.com>
2024-11-05 10:02:56 +0530

    Minor fix

--- src/components/OptionsDetails/index.tsx
+++ src/components/OptionsDetails/index.tsx
@@ -64,7 +64,6 @@ const OptionsDetails: React.FC<OptionsDetailsProps> = ({ career }) => {
                   `/career-options/flow-chart?careerOption=${card.choice}`
                 );
               }}
-              className="inline-block px-6 py-2 text-sm font-medium text-white bg-blue-600 rounded-md shadow-md hover:bg-blue-700 transition-colors duration-200"
             >
               Learn More
             </Button>
