24c4d3a61be921f7fe411385404d2425fb13b49e
Krishnama Naidu <krishnamkrish67@gmail.com>
2024-12-15 16:13:39 +0530

    Minor changes

--- src/components/NotificationCard/index.tsx
+++ src/components/NotificationCard/index.tsx
@@ -16,13 +16,10 @@ const NotificationCard: React.FC<NotificationCardProps> = ({
       <div className="flex items-center mb-2 sm:mb-4">
         <FontAwesomeIcon icon={faBell} className="text-yellow-500" />
         <h3 className="text-lg sm:text-xl font-semibold text-gray-800 ml-2">
-          Exciting Updates Coming Soon!
+          {title}
         </h3>
       </div>
-      <p className="text-gray-600 text-sm sm:text-base">
-        We currently have few career choices available as of now. Stay tuned for
-        more options that will be added in the future!
-      </p>
+      <p className="text-gray-600 text-sm sm:text-base">{description}</p>
     </div>
   );
 };
