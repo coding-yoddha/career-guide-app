9785e6f2df55539d28189e59273d58f977fc48c9
Krishnama Naidu <krishnamkrish67@gmail.com>
2024-12-17 15:26:32 +0530

    Minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,44 +37,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>Agla Kadam</title>
-        <meta
-          name="description"
-          content="Discover different career paths after 10th class. Get step-by-step guidance to pursue your dream career in engineering, medicine, arts, and more!.One stop destination to know what to choose after your 10th Standard"
-        />
-        <meta
-          name="keywords"
-          content="career guidance, after 10th class, career options, engineering, medicine, law, arts"
-        />
-        <meta name="robots" content="index, follow" />
-
-        {/* Open Graph Meta Tags */}
-        <meta
-          property="og:title"
-          content="Agla Kadam - Your Roadmap to a Bright Future"
-        />
-        <meta
-          property="og:description"
-          content="Explore career options after 10th class and find your path with Agla Kadam."
-        />
-        <meta property="og:type" content="website" />
-        {/* Schema Markup (JSON-LD) */}
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: `
-              {
-                "@context": "https://schema.org",
-                "@type": "WebPage",
-                "name": "Agla Kadam - Career Guidance After 10th Class",
-                "description": "Agla Kadam helps students explore career options after 10th class with clear steps for various careers like engineering, medicine, law, and more.",
-                "url": "https://aglakadam.com"
-              }
-            `,
-          }}
-        />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
